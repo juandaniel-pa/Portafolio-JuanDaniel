b4767d5f26b68ea3dfb6f33af479a08b5377679e
juandaniel <jddj1897@gmail.com>
2021-10-25 04:39:10 -0500

    Swiper, images for portfolio, styles and more added

--- JS/main.js
+++ JS/main.js
@@ -95,7 +95,18 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
